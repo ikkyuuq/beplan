d797a1ed54b8a8fd1b73665a413d8e3cd3e95bbb
Poopamaster <poopazizi16@gmail.com>
2025-02-06 02:02:09 +0700

    create Homepage web

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -7,29 +7,37 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <nav role='navigation'>
+          <div className='Menu'>
+            <ul>
+              <li><a href="" className='Home'>Home page</a></li>
+              <li><a href="" className='AboutMe'>About page</a></li>
+              <li><a href="" className='Home'>Donation</a></li>
+              <li><a href="" className='FAQ'>FAQ</a></li>
+              <li><button className="Download-page">เปิด Planner</button></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+
+    <div className='body-header'>
+      <h1>จัดตารางเวลาด้วย Planner</h1>
+      <h2>ซอฟต์แวร์นี้ออกแบบมาเพื่อช่วยให้ผู้ใช้วางแผนและบรรลุเป้าหมายได้อย่างมีประสิทธิภาพโดยใช้ SMART GOAL</h2>
+      <h1>SMART GOAL</h1>
+      <h2>ระบบ SMART เพื่อให้เป้าหมายสามารถนําไปใช้ได้จริงและวัดผลได้ หลีกเลี่ยงไม่ให้ผู้ใช้ต้องใช้หลายแอพ</h2>
+      <h1>Template & Tracker</h1>
+      <h2>เรามี เทมเพลต และ เคล็ดลับสําเร็จรูป เพื่อสร้างเป้าหมายได้ง่ายขึ้น</h2>
+      <h2>และติดตามพฤติกรรมของผู้ใช้เพื่อนําเสนอความคืบหน้าผ่านกราฟที่แสดงภาพเพื่อให้แน่ใจว่ามีการบรรลุเป้าหมายอย่างต่อเนื่อง</h2>
+    </div>
+    <div className="button-download">
+    <button href= "" className="Download">Download</button>
+    <button href= "" className="sign-in">เข้าสู่ระบบ</button>
+    </div>
+    </div>
+    
+  );
 }
 
 export default App
