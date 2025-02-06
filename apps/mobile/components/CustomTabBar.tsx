4d281444fa3e2a58e73e943f4c9ed738f24fee14
ikkyuuq <kittipongprasompong@gmail.com>
2025-02-06 21:20:50 +0700

    refactor(mobile): refactor component and added animate to tabbar when switch tab

--- apps/mobile/components/CustomTabBar.tsx
+++ apps/mobile/components/CustomTabBar.tsx
@@ -1,147 +1,114 @@
-import { View, Platform } from "react-native";
-import { useLinkBuilder, useTheme } from "@react-navigation/native";
-import { Text, PlatformPressable } from "@react-navigation/elements";
-import {
-  createBottomTabNavigator,
-  BottomTabBarProps,
-} from "@react-navigation/bottom-tabs";
-import { Entypo, Feather } from "@expo/vector-icons";
-
-type TabButtonProps = {
-  route: any;
-  isFocused: boolean;
-  navigation: any;
-  descriptors: any;
-  buildHref: (routeName: string, params?: object) => string;
-  icon: any;
-};
-
-const TabButton = ({
-  route,
-  isFocused,
-  navigation,
-  descriptors,
-  buildHref,
-  icon,
-}: TabButtonProps) => {
-  const { options } = descriptors[route.key];
-
-  const onPress = () => {
-    const event = navigation.emit({
-      type: "tabPress",
-      target: route.key,
-      canPreventDefault: true,
-    });
-
-    if (!isFocused && !event.defaultPrevented) {
-      navigation.navigate(route.name, route.params);
-    }
-  };
-
-  const onLongPress = () => {
-    navigation.emit({
-      type: "tabLongPress",
-      target: route.key,
-    });
-  };
-
-  return (
-    <PlatformPressable
-      key={route.name}
-      href={buildHref(route.name, route.params)}
-      accessibilityState={isFocused ? { selected: true } : {}}
-      accessibilityLabel={options.tabBarAccessibilityLabel}
-      testID={options.tabBarButtonTestID}
-      onPress={onPress}
-      onLongPress={onLongPress}
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 5,
-        backgroundColor: isFocused ? "#000" : "#fff",
-        borderRadius: 15,
-        aspectRatio: 1,
-      }}
-    >
-      {icon[route.name](isFocused ? { color: "#fff" } : { color: "#000" })}
-    </PlatformPressable>
-  );
-};
+import { View, StyleSheet, LayoutChangeEvent } from "react-native";
+import TabBarButton from "./TabBarButton";
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import { useEffect, useState } from "react";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
 function CustomTabBar({ state, descriptors, navigation }: BottomTabBarProps) {
-  const { buildHref } = useLinkBuilder();
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const buttonWidth = dimensions.width / state.routes.length;
+  const tabPosX = useSharedValue(0);
 
-  const icon = {
-    schedule: (props: any) => <Feather name="calendar" size={24} {...props} />,
-    analysis: (props: any) => <Entypo name="line-graph" size={24} {...props} />,
-    create: (props: any) => <Feather name="plus-circle" size={24} {...props} />,
-    community: (props: any) => <Feather name="users" size={24} {...props} />,
+  const onTabBarLayout = (e: LayoutChangeEvent) => {
+    setDimensions({
+      width: e.nativeEvent.layout.width,
+      height: e.nativeEvent.layout.height,
+    });
   };
 
-  const { mainRoutes, communityRoute } = state.routes.reduce(
-    (acc, route) => {
-      if (route.name === "community") {
-        acc.communityRoute = route;
-      } else {
-        acc.mainRoutes.push(route);
-      }
-      return acc;
-    },
-    { mainRoutes: [], communityRoute: null },
-  );
+  const animateTabBar = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: tabPosX.value }],
+    };
+  });
 
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        position: "absolute",
-        bottom: 40,
-        marginHorizontal: 100,
-        gap: 5,
-      }}
-    >
-      <View
-        style={{
-          flexDirection: "row",
-          borderRadius: 15,
-          backgroundColor: "#fff",
-          flex: 1,
-          gap: 3,
-        }}
-      >
-        {mainRoutes.map((route, index) => (
-          <TabButton
-            key={route.name}
-            route={route}
-            isFocused={state.index === index}
-            navigation={navigation}
-            descriptors={descriptors}
-            buildHref={buildHref}
-            icon={icon}
-          />
-        ))}
-      </View>
+    <View onLayout={onTabBarLayout} style={styles.tabbar}>
+      <Animated.View
+        style={[
+          animateTabBar,
+          {
+            borderRadius: 20,
+            position: "absolute",
+            backgroundColor: "#000",
+            width: buttonWidth,
+            height: dimensions.height,
+          },
+        ]}
+      />
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
 
-      {communityRoute && (
-        <View
-          style={{
-            flexDirection: "row",
-            flex: 1 / 3,
-          }}
-        >
-          <TabButton
-            route={communityRoute}
-            isFocused={state.index === state.routes.length - 1}
-            navigation={navigation}
-            descriptors={descriptors}
-            buildHref={buildHref}
-            icon={icon}
+        useEffect(() => {
+          tabPosX.value = withSpring(buttonWidth * state.index, {
+            mass: 1,
+            damping: 20,
+            stiffness: 200,
+            overshootClamping: false,
+            restDisplacementThreshold: 0.01,
+            restSpeedThreshold: 2,
+          });
+        }, [state.index]);
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <TabBarButton
+            key={route.name}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            options={options}
+            isFocused={isFocused}
+            route={route}
+            label={route.name}
           />
-        </View>
-      )}
+        );
+      })}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  tabbar: {
+    position: "absolute",
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    marginHorizontal: 90,
+    justifyContent: "space-between",
+    alignItems: "center",
+    bottom: 50,
+    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowRadius: 10,
+    shadowOpacity: 0.1,
+  },
+});
 
 export default CustomTabBar;
---
+++ apps/mobile/components/TabBarButton.tsx
@@ -0,0 +1,75 @@
+import { useLinkBuilder } from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
+import { Pressable, StyleSheet } from "react-native";
+import Animated, {
+  interpolateColor,
+  useAnimatedProps,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import { useEffect } from "react";
+
+type TabBarButtonProps = {
+  options: any;
+  route: any;
+  isFocused: boolean;
+  onPress: () => void;
+  onLongPress: () => void;
+};
+
+const AnimatedFeather = Animated.createAnimatedComponent(Feather);
+
+export default function TabBarButton({
+  options,
+  route,
+  isFocused,
+  onPress,
+  onLongPress,
+}: TabBarButtonProps) {
+  const buildHref = useLinkBuilder;
+
+  const icon = {
+    schedule: (props: any) => <AnimatedFeather name="calendar" {...props} />,
+    analysis: (props: any) => <AnimatedFeather name="bar-chart-2" {...props} />,
+    create: (props: any) => <AnimatedFeather name="plus-circle" {...props} />,
+    community: (props: any) => <AnimatedFeather name="users" {...props} />,
+  };
+
+  const scale = useSharedValue(0);
+
+  useEffect(() => {
+    scale.value = withSpring(isFocused ? 1 : 0);
+  }, [scale, isFocused]);
+
+  const animatedProps = useAnimatedProps(() => {
+    return {
+      color: interpolateColor(scale.value, [0, 1], ["#222", "#fff"]),
+    };
+  });
+
+  return (
+    <Pressable
+      href={buildHref(route.name, route.params)}
+      accessibilityState={isFocused ? { selected: true } : {}}
+      accessibilityLabel={options.tabBarAccessibilityLabel}
+      testID={options.tabBarButtonTestID}
+      onPress={onPress}
+      onLongPress={onLongPress}
+      style={styles.tabBarButton}
+    >
+      {icon[route.name]({
+        size: 24,
+        animatedProps: animatedProps,
+      })}
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  tabBarButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    aspectRatio: 1,
+  },
+});
