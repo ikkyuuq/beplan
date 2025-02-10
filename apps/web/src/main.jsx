9722419a1788897dbb7206136ae70f41d9b98ed5
Poopamaster <poopazizi16@gmail.com>
2025-02-11 00:07:16 +0700

    edit mainjsx

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import App from "./App";
-import Home from "./Home";
-import AboutMe from "./Aboutme";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
