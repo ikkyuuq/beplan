0f0d94f01c0cbef866a72d37dee74be830a0b089
Poopamaster <poopazizi16@gmail.com>
2025-02-20 17:32:47 +0700

    create interface admin backend

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -2,8 +2,9 @@ import { BrowserRouter as Router, Route, Routes, Link, Outlet } from "react-rout
 import Home from "./Home";
 import AboutMe from "./AboutMe";
 import Donate from "./Donate";
-import "./Home.css";
 import FAQ from "./FAQ";
+import "./Home.css"; // ใช้กับ Layout หลัก
+import AdminDashboard from "./Admindashboard";
 
 const Layout = () => {
   return (
@@ -12,21 +13,20 @@ const Layout = () => {
         <nav>
           <ul>
             <div className="logo-container">
-            <img src="https://img2.pic.in.th/pic/Screenshot-2025-02-11-215739.png" alt="Logo" border="0" />
-            <a href="/">SMART GOAL</a>
+              <img src="https://img2.pic.in.th/pic/Screenshot-2025-02-11-215739.png" alt="Logo" border="0" />
+              <a href="/">SMART GOAL</a>
             </div>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/aboutme">About Me</Link></li>
             <li><Link to="/donate">Donate</Link></li>
             <li><Link to="/faq">FAQ</Link></li>
             <button className="start">GET STARTED!</button>
-            </ul>
+          </ul>
         </nav>
       </header>
       <main>
         <Outlet />
       </main>
-    
     </div>
   );
 };
@@ -35,12 +35,16 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Layout หลักสำหรับ User */}
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="aboutme" element={<AboutMe />} />
           <Route path="donate" element={<Donate />} />
           <Route path="faq" element={<FAQ />} />
         </Route>
+
+        {/* Route สำหรับ Admin แยกออกมา */}
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
