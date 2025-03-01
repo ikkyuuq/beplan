9aa35e9e5a3d116cc69b5cc705a6b3b229814f43
ikkyuuq <kittipongprasompong@gmail.com>
2025-03-01 07:12:59 +0700

    feat(backend): using prefix to template router

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
 
 from database import lifespan
-from routers import ai, goals, fetching, template
+from routers import ai, fetching, goals, template
 
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(ai.router, prefix="/ai")
 app.include_router(goals.router)
 app.include_router(fetching.router)
-app.include_router(template.router)
+app.include_router(template.router, prefix="/template")
+
 
 @app.get("/get")
 def read_root():
     return {"message": "Hello from Backend"}
 
+
 @app.put("/update")
 def delete_root():
-    return {"message":"this is update methods"}
+    return {"message": "this is update methods"}
